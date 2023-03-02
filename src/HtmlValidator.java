23e8a7b3d0e3946c9061155a959980df8acfa926
mateo <mateozacar@gmail.com>
2023-03-01 21:05:46 -0500

    Initial commit

---
+++ src/HtmlValidator.java
@@ -0,0 +1,16 @@
+import java.util.Queue;
+import java.util.Stack;
+
+
+public class HtmlValidator {
+
+	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+
+		/* IMPLEMENT THIS METHOD! */
+
+		return null; // this line is here only so this code will compile if you don't modify it
+	}
+	
+
+}
+
