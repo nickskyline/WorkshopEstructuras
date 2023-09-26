19c6165d1a0e5bd4a02a82d7790d79a5973bc975
ad-nicolas <duquenicolas391@gmail.com>
2023-09-26 14:41:34 -0500

    Ejercicio Agregado

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,16 +1,78 @@
 import java.util.Queue;
 import java.util.Stack;
 
-
 public class HtmlValidator {
-
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+		Stack<HtmlTag> htmlStacks = new Stack<>();
 
-		/* IMPLEMENT THIS METHOD! */
-
-		return null; // this line is here only so this code will compile if you don't modify it
+		for (HtmlTag tag : tags) {
+			if (tag.isOpenTag()) {
+				if (htmlStacks.isEmpty() || !htmlStacks.peek().matches(tag)) {
+					htmlStacks.push(tag);
+				}
+			} else if (!tag.isSelfClosing()) {
+				if (htmlStacks.isEmpty()) {
+					return null;
+				}
+				if (htmlStacks.peek().matches(tag)) {
+					htmlStacks.pop();
+				} else {
+					Stack<HtmlTag> tagsCoincididos = new Stack<>();
+					for (HtmlTag tagStack : htmlStacks) {
+						if (tagStack.matches(tag)) {
+							tagsCoincididos.push(tag);
+						}
+					}
+					if (!tagsCoincididos.isEmpty()) {
+						return htmlStacks;
+					}
+				}
+			}
+		}
+		return htmlStacks;
 	}
-	
+	/*
+	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+		Stack<HtmlTag> htmlStacks = new Stack<>();
+		Stack<HtmlTag> htmlTags = new Stack<>();
+		htmlTags.addAll(tags);
+		for (HtmlTag tag : tags) {
+			if (tag.isOpenTag()){
+				if (htmlStacks.isEmpty()){
+					htmlStacks.push(tag);
+				}else{
+					for (HtmlTag tagStack: htmlStacks) {
+						if (!tagStack.matches(tag)){
+							htmlStacks.push(tag);
+							break;
+						}
+					}
+				}
+
+			}else{
+				if (!tag.isSelfClosing()){
+					if(!htmlStacks.isEmpty()){
+						if (htmlStacks.peek().matches(tag)){
+							htmlStacks.pop();
+						}else{
+							Stack<HtmlTag> tagsCoincididos = new Stack<>();
+							for (HtmlTag tagStack: htmlStacks) {
+								if (tagStack.matches(tag)){
+										tagsCoincididos.push(tag);
+								}
+							}
+							if (!tagsCoincididos.isEmpty()){
+								return htmlStacks;
+							}
+						}
+					}else{
+						return null;
+					}
+				}
+			}
+		}
+		return htmlStacks; // this line is here only so this code will compile if you don't modify it
+	}
+	 */
 
 }
-
